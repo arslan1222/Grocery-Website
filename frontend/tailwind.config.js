b07241319c92bafad7e7daede42daf9182dead7f
Arslan Haider <arslanhaidermughal8@gmail.com>
2025-04-11 11:54:21 +0500

    Navigateion bar added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#4fbf8b",
+        "primary-dull": "#44ae7c",
+      }
+    },
   },
   plugins: [],
 }
